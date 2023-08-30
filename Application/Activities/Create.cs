3cf30abeafa4ef893cfa5055d72759a04fcd832e
Ahmed <ahmedadel_0792@yahoo.com>
2023-08-30 18:27:27 +0300

    End of Section 4

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }    
+        }
+
+        public class Hanlder : IRequestHandler<Command>
+        {
+            private DataContext _context;
+
+            public Hanlder(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                await _context.Activities.AddAsync(request.Activity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Application/Activities/List.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Hanlder : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Hanlder(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+
+}
